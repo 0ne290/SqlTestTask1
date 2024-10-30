30ac2d524d9095f2f91c65268ecf33f67c43c0c5
Алексей <97665299+0ne290@users.noreply.github.com>
2024-10-30 13:25:22 +0800

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -5,7 +5,7 @@ USE TestTask;
 
 CREATE TABLE Td
 (
-	ContractNumber CHAR(6) NOT NULL,
+  ContractNumber CHAR(6) NOT NULL,
   INDEX ContractNumberIndex (ContractNumber),
   TdCode SMALLINT UNSIGNED NOT NULL PRIMARY KEY,
   Service VARCHAR(64) NOT NULL,
@@ -16,33 +16,33 @@ CREATE TABLE Td
 
 CREATE TABLE EMail
 (
-	ContractNumber CHAR(6) NOT NULL,
+  ContractNumber CHAR(6) NOT NULL,
   FOREIGN KEY (ContractNumber) REFERENCES Td(ContractNumber),
-  Service VARCHAR(64) NOT NULL
+  EMail VARCHAR(64) NOT NULL
 );
 
 
 INSERT INTO Td VALUES
-	('111111', 1, 'ВЛ', FALSE, NULL),
-	('222222', 2, 'ВЛ', FALSE, NULL),
-	('222222', 3, 'ДМФ', FALSE, NULL),
-	('222222', 4, 'IPTV', FALSE, NULL),
-	('222222', 5, 'IPTV', TRUE, NULL),
-	('222222', 6, 'Видеосервис', TRUE, NULL),
-	('222222', 7, 'Видеосервис', TRUE, NULL),
-	('222222', 8, 'Оборудование', TRUE, NULL),
-	('222222', 9, 'КТВ', FALSE, NULL),
-	('222222', 10, 'Оборудование', TRUE, NULL),
-	('222222', 11, 'Оборудование', TRUE, NULL),
-	('333333', 888, 'Пакет', FALSE, NULL),
+  ('111111', 1, 'ВЛ', FALSE, NULL),
+  ('222222', 2, 'ВЛ', FALSE, NULL),
+  ('222222', 3, 'ДМФ', FALSE, NULL),
+  ('222222', 4, 'IPTV', FALSE, NULL),
+  ('222222', 5, 'IPTV', TRUE, NULL),
+  ('222222', 6, 'Видеосервис', TRUE, NULL),
+  ('222222', 7, 'Видеосервис', TRUE, NULL),
+  ('222222', 8, 'Оборудование', TRUE, NULL),
+  ('222222', 9, 'КТВ', FALSE, NULL),
+  ('222222', 10, 'Оборудование', TRUE, NULL),
+  ('222222', 11, 'Оборудование', TRUE, NULL),
+  ('333333', 888, 'Пакет', FALSE, NULL),
   ('333333', 12, 'ВЛ', FALSE, 888),
-	('333333', 13, 'IPTV', FALSE, 888),
-	('333333', 14, 'IPTV', FALSE, 888),
-	('333333', 15, 'IPTV', FALSE, 888),
-	('333333', 16, 'IPTV', FALSE, 888),
-	('333333', 17, 'Оборудование', TRUE, NULL),
-	('333333', 18, 'КТВ', FALSE, NULL);
+  ('333333', 13, 'IPTV', FALSE, 888),
+  ('333333', 14, 'IPTV', FALSE, 888),
+  ('333333', 15, 'IPTV', FALSE, 888),
+  ('333333', 16, 'IPTV', FALSE, 888),
+  ('333333', 17, 'Оборудование', TRUE, NULL),
+  ('333333', 18, 'КТВ', FALSE, NULL);
 
 INSERT INTO EMail VALUES
-	('111111', '111111@mail.ru'),
+  ('111111', '111111@mail.ru'),
   ('333333', '333333@mail.ru');
